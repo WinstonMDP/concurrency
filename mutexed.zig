6fb053f448fc0e9f9419ef25ddb0eb0913d4d370
WinstonMDP <skrimer.swat@bk.ru>
2025-03-16 09:30:14 +0300

    feat: add mutexed

---
+++ mutexed.zig
@@ -0,0 +1,25 @@
+const std = @import("std");
+const Mutex = std.Thread.Mutex;
+
+fn Mutexed(T: type) type {
+    return struct {
+        val: T,
+        mutex: Mutex = Mutex{},
+
+        fn init(val: T) @This() {
+            return @This(){ .val = val };
+        }
+
+        fn acquire(self: *@This()) *T {
+            self.mutex.lock();
+            return &self.val;
+        }
+    };
+}
+
+test {
+    var mutexed = Mutexed(u8).init(9);
+    const val = mutexed.acquire();
+    try std.testing.expectEqual(9, val.*);
+    defer mutexed.mutex.unlock();
+}
